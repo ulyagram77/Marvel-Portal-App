736aa33ca64c0a2808b1836e800791ab183445c7
whitegolyb <kirillulianow@gmail.com>
2024-01-07 18:25:13 +0200

    hotfix: picture styles corrected

--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -73,12 +73,22 @@ class CharInfo extends Component {
 
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+    let imgStyle = { objectFit: 'cover' };
+    if (
+        thumbnail ===
+        'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+    ) {
+        imgStyle = { objectFit: 'contain' };
+    }
+
     return (
         <>
             <div className="char__basics">
                 <img
                     src={thumbnail}
                     alt={name}
+                    style={imgStyle}
                 />
                 <div>
                     <div className="char__info-name">{name}</div>
@@ -101,7 +111,11 @@ const View = ({ char }) => {
             <div className="char__descr">{description}</div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
+                {comics.length > 0
+                    ? null
+                    : 'There are no comics with this character =('}
                 {comics.map((item, i) => {
+                    if (i > 9) return;
                     return (
                         <li
                             key={i}

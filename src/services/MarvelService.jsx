6fc518eef23969bc13172c14571090f54683ff80
whitegolyb <kirillulianow@gmail.com>
2024-01-07 16:39:49 +0200

    added access to comics

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -38,6 +38,7 @@ class MarvelService {
                 character.thumbnail.path + `.${character.thumbnail.extension}`,
             homepage: character.urls[0].url,
             wiki: character.urls[1].url,
+            comics: character.comics.items,
         };
     };
 }

631acbd2a99d96e4a9a2c7b0468d9c830872ff82
whitegolyb <kirillulianow@gmail.com>
2024-01-07 15:30:43 +0200

    added loader before randChar is loaded

--- src/components/charRandom/CharRandom.jsx
+++ src/components/charRandom/CharRandom.jsx
@@ -30,6 +30,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharacterLoaded)
@@ -38,6 +39,10 @@ class RandomChar extends Component {
 
     onCharacterLoaded = (char) => {
         this.setState({ char, loading: false });
+    };
+
+    onCharLoading = () => {
+        this.setState({ loading: true });
     };
 
     onError = () => {

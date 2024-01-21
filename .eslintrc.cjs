2abb08bf1f4dba137018848dcde551ede939b8ea
whitegolyb <kirillulianow@gmail.com>
2024-01-21 15:13:56 +0200

    configured eslint + prettier, added codestyle scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'no-var': 'error',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
   },
-}
+};

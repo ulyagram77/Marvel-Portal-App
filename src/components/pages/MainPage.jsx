feb1358746754a7240b18d6488f9218d08dc4e07
whitegolyb <kirillulianow@gmail.com>
2024-01-24 13:26:07 +0200

    feat: created several pages for routing

---
+++ src/components/pages/MainPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import CharRandom from '../charRandom/CharRandom';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+
+import decoration from '../../assets/vision.png';
+
+const MainPage = () => {
+    const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+    const onCharacterSelected = id => {
+        setSelectedCharacter(id);
+    };
+
+    return (
+        <>
+            <ErrorBoundary>
+                <CharRandom />
+            </ErrorBoundary>
+            <div className="char__content">
+                <ErrorBoundary>
+                    <CharList onCharacterSelected={onCharacterSelected} />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <CharInfo characterId={selectedCharacter} />
+                </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision" />
+        </>
+    );
+};
+
+export default MainPage;

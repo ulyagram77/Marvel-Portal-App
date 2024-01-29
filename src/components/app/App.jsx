87f683172c34e6fe3ad79bf3222432ef1b01afb3
whitegolyb <kirillulianow@gmail.com>
2024-01-29 02:23:14 +0200

    changed to browser router component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
 import AppHeader from '../appHeader/AppHeader';

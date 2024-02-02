82415aa19f45ba0cb5ae48c903908fe3455bbfff
whitegolyb <kirillulianow@gmail.com>
2024-02-02 02:08:55 +0200

    added aliases for import paths

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            src: '/src',
+            components: '/src/components',
+            pages: '/src/components/pages',
+            assets: '/src/assets',
+            hooks: '/src/hooks',
+            services: '/src/services',
+            styles: '/src/styles',
+        },
+    },
 });

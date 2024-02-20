7f177e9e07f5a0d615dff0f1e3b4455edb4af523
Whitegolyb <kirillulianow@gmail.com>
2024-02-20 15:02:38 +0200

    added layouts for SinglePage component wich should be render it optionally

---
+++ src/components/interface/singleComicLayout/SingleComicLayout.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './SingleComicLayout.scss';
+
+const SingleComicLayout = ({ data }) => {
+    const { title, description, pageCount, thumbnail, language, price } = data;
+
+    return (
+        <div className="single-comic">
+            <img src={thumbnail} alt={title} className="single-comic__img" />
+            <div className="single-comic__info">
+                <h2 className="single-comic__name">{title}</h2>
+                <p className="single-comic__descr">{description}</p>
+                <p className="single-comic__descr">{pageCount}</p>
+                <p className="single-comic__descr">Language: {language}</p>
+                <div className="single-comic__price">{price}</div>
+            </div>
+            <Link to="/comics" className="single-comic__back">
+                Back to all
+            </Link>
+        </div>
+    );
+};
+
+SingleComicLayout.propTypes = {
+    data: PropTypes.object.isRequired,
+};
+
+export default SingleComicLayout;

342d79f5cfd0bb8dd8c0b300d99a7f4c2bf29d9c
kyrylo <kirillulianow@gmail.com>
2024-08-05 16:11:08 +0300

    added close handler for menu

--- src/components/others/menu/Menu.jsx
+++ src/components/others/menu/Menu.jsx
@@ -2,13 +2,35 @@ import PropTypes from 'prop-types';
 
 import { NavLink } from 'react-router-dom';
 import { MenuIcon } from '..';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 import './Menu.scss';
 
 const Menu = ({ open, setOpen, links, isLinkActive }) => {
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = event => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpen(false);
+            }
+        };
+
+        if (open) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [open, setOpen]);
+
     const clazz = `menu__body ${open ? 'active' : ''}`;
 
     return (
-        <nav className="menu">
+        <nav className="menu" ref={menuRef}>
             <MenuIcon open={open} setOpen={setOpen} />
 
             <ul className={clazz}>
